001741581a8e497a15e0bb4a7244b0e34699c956
David <aldavid342@gmail.com>
2025-12-03 15:23:04 +0100

    💄 F03: LOGIN > LoginForm > Input > Add styling

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router';
 import styled from "styled-components"
+import { BsPersonCircle } from "react-icons/bs"
 
 export default function LoginForm() {
   
@@ -28,13 +29,16 @@ export default function LoginForm() {
         <h2>Connectez-vous</h2>
       </div>
       <div>
-        <input
-          value={inputValue}
-          onChange={handleChange}
-          type="text"
-          placeholder="Entrez votre prénom"
-          required
-        />
+        <div className='input-with-icon'>
+          <BsPersonCircle className='icon'/>
+          <input
+            value={inputValue}
+            onChange={handleChange}
+            type="text"
+            placeholder="Entrez votre prénom"
+            required
+          />
+        </div>
         <button>Accédez à mon espace</button>
       </div>
     </LoginFormStyled>
@@ -49,8 +53,8 @@ const LoginFormStyled = styled.form`
   margin: 0px auto;
   padding: 2.5rem 2rem;
   border-radius: 5px;
-  font-family: "Amatic SC", cursive;
-  
+  font-family: 'Amatic SC', cursive;
+
   hr {
     border: 1.5px solid #f56a2c;
     margin-bottom: 40px;
@@ -66,5 +70,32 @@ const LoginFormStyled = styled.form`
     margin: 20px 10px 10px;
     color: white;
     font-size: 36px;
+  }
+
+  .input-with-icon {
+    background-color: #fff;
+    border-radius: 5px;
+    display: flex;
+    align-items: center;
+    padding: 18px 24px;
+    margin: 18px 0;
+
+    .icon {
+      font-size: 15px;
+      margin-right: 8px;
+      color: #93a2b1;
+    }
+
+    input {
+      border: none;
+      font-size: 15px;
+      color: #17161a;
+      background: white;
+    }
+
+    &::placeholder {
+      background: white;
+      color: lightgray;
+    }
   }
 `;
