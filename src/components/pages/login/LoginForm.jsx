6dcbeda9a664e8989589670c8b66d651f4ffd2f2
David <aldavid342@gmail.com>
2025-12-04 16:21:04 +0100

    ♻️ F03: LOGIN > LoginForm > Create reusable <Input/>

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router';
 import styled from 'styled-components';
+
 import { BsPersonCircle } from 'react-icons/bs';
 import { IoChevronForward } from 'react-icons/io5';
+import Input from './Input';
 
 export default function LoginForm() {
   // state
@@ -29,19 +31,17 @@ export default function LoginForm() {
         <h2>Connectez-vous</h2>
       </div>
       <div>
-        <div className="input-with-icon">
-          <BsPersonCircle className="icon" />
-          <input
-            value={inputValue}
-            onChange={handleChange}
-            type="text"
-            placeholder="Entrez votre prénom"
-            required
-          />
-        </div>
-        <button className='button-with-icon'>
+        <Input
+          value={inputValue}
+          onChange={handleChange}
+          placeholder={'Entrez votre prénom'}
+          required
+          Icon={<BsPersonCircle className="icon" />}
+        />
+
+        <button className="button-with-icon">
           <span>Accédez à mon espace</span>
-          <IoChevronForward className='icon'/>
+          <IoChevronForward className="icon" />
         </button>
       </div>
     </LoginFormStyled>
@@ -72,33 +72,6 @@ const LoginFormStyled = styled.form`
     margin: 20px 10px 10px;
     color: white;
     font-size: 36px;
-  }
-
-  .input-with-icon {
-    background-color: #fff;
-    border-radius: 5px;
-    display: flex;
-    align-items: center;
-    padding: 18px 24px;
-    margin: 18px 0;
-
-    .icon {
-      font-size: 15px;
-      margin-right: 8px;
-      color: #93a2b1;
-    }
-
-    input {
-      border: none;
-      font-size: 15px;
-      color: #17161a;
-      background: white;
-    }
-
-    &::placeholder {
-      background: white;
-      color: lightgray;
-    }
   }
 
   .button-with-icon {
